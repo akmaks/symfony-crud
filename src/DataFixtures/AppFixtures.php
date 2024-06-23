8e688ed08efd124aaffe32bc3adee52de7c5813a
Akim Maksimau <akim.maksimau@veeam.com>
2024-06-23 20:38:22 +0200

    add product entity

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 50; $i++) {
+            $product = new Product();
+            $product->setName($faker->name);
+            $product->setPrice($faker->randomFloat());
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
